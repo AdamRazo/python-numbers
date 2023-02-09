fcf021bbb84ab4b53fd8450e40eb84ba24b1c11d
AdamRazo <122961294+AdamRazo@users.noreply.github.com>
2023-02-08 18:49:22 -0700

    random number

--- numbers.py
+++ numbers.py
@@ -1,1 +1,13 @@
 # Python Numbers
+aNumber = 10 # created dattype int
+print(type(aNumber))
+
+aNumber = ("10")
+afloatnumber = 1.2
+#aNumber = float(aNumber)
+
+#print(aNumber + afloatnumber)
+
+import random
+
+print(random.randrange(0,1000))
